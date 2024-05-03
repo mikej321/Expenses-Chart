e527601f3b826f34802267414f27c8aa8eb30257
Michael Johnson <michaeljcantley@gmail.com>
2024-05-03 12:53:18 -0400

    Finish styling and recharts functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,74 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 import './App.css'
+import data from './assets/data.json'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Balance = () => {
+  return (
+    <header className="balance_section">
+      <div className="balance_amount">
+        <p>My balance</p>
+        <p>$921.48</p>
+      </div>
+      <svg width="40" height="48" viewBox="0 0 72 50" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><circle fill="#382314" cx="48" cy="24" r="24"/><circle stroke="#FFF" stroke-width="2" cx="24" cy="24" r="23"/></g></svg>
+    </header>
+  )
+}
+
+const CustomToolTip = ({ active, payload, label }) => {
+  if (active && payload?.length) {
+    return (
+      <div>
+        {payload.map((ele, index) => (
+          <>
+            <p>{`$${ele.value}`}</p>
+          </>
+        ))}
+      </div>
+    )
+  }
+  return null;
+}
+
+const Chart = ({ todayDate }) => {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="chart_section">
+      <h1>Spending - Last 7 days</h1>
+      <ResponsiveContainer width="100%" height={275}>
+        <BarChart width={730} height={250} data={data}>
+          <XAxis dataKey="day" axisLine={false} tickLine={false} tickMargin={7} fontSize="12px" />
+          <Tooltip cursor={false} content={<CustomToolTip />} wrapperStyle={{ padding: "1rem", background: "hsl(25, 47%, 15%)", borderRadius: "8px", color: "hsl(33, 100%, 98%)", fontWeight: "bold", fontSize: "1.2rem"}} position={{ y: -10 }} offset={-30} />
+          <Bar dataKey="amount" fill="hsl(10, 79%, 65%)" radius={4}>
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={ todayDate === index + 1 ? "hsl(10, 79%, 65%)" : "hsl(186, 34%, 60%)" } />
+            ))}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
+      
+      <div className="total_container">
+        <div className="current_month_total">
+          <p>Total this month</p>
+          <p>$478.33</p>
+        </div>
+        <div className="previous_month_difference">
+          <p>+2.4%</p>
+          <p>from last month</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
+  )
+}
+
+function App() {
+  const [day, getDay] = useState(new Date().getDay());
+
+  return (
+    <div className="container">
+      <Balance />
+      <Chart todayDate={day} />
+    </div>
   )
 }
 
